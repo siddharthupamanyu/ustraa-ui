e2e02796a276f76e3ddfa31f97c26f892b9d9214
munish13490 <munisiiii@gmail.com>
2020-01-06 00:32:02 +0530

    Updates

---
+++ precache-manifest.aec04ac7f54517078b7748ff03179dde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da21b58ab11c863587f93fd30971111",
+    "url": "/ustraa-ui/index.html"
+  },
+  {
+    "revision": "1e0e845bf94253091da7",
+    "url": "/ustraa-ui/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ed169cfb7afae02664ec",
+    "url": "/ustraa-ui/static/js/2.9a054d7e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ustraa-ui/static/js/2.9a054d7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e0e845bf94253091da7",
+    "url": "/ustraa-ui/static/js/main.b6a748da.chunk.js"
+  },
+  {
+    "revision": "0cb4b3ed05207684886a",
+    "url": "/ustraa-ui/static/js/runtime-main.f2707982.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ustraa-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
